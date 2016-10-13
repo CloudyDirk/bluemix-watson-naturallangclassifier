b342670bf605a510d4ba6fe663dc629b4424878a
CloudyDirk <d.guenther@cloud57.de>
2016-10-13 09:01:03 -0400

    whole app

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+watson: {
+natural_language_classifier: {
+username: "afe66fcf-db20-416b-ad89-de8bd62dac2d",
+password: "St1t2r15tG4M",
+version: "v1",
+id: "2d7aefx102-nlc-10182"
+}
+}
+};
+module.exports = config;
